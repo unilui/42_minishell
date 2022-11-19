3849cee606620bbf1ad9f717f4ffb6b04b7753c1
Felipe Camargo <lufelip2@student.42sp.org.br>
2022-11-19 14:24:30 -0300

    .

--- builtin/builtin_heredoc.c
+++ builtin/builtin_heredoc.c
@@ -6,12 +6,20 @@
 /*   By: lufelip2 <lufelip2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:31:18 by lufelip2          #+#    #+#             */
-/*   Updated: 2022/11/08 08:47:45 by lufelip2         ###   ########.fr       */
+/*   Updated: 2022/11/19 14:22:22 by lufelip2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
+
+void	i_hate_u_norme(char **table)
+{
+	if (table)
+		send_buffer(table, g_data.pipe_out[1], TRUE);
+	if (table)
+		free_table(table);
+}
 
 void	builtin_heredoc(char **args)
 {
@@ -35,8 +43,7 @@ void	builtin_heredoc(char **args)
 			free(tmp);
 		line = readline("> ");
 	}
-	send_buffer(table, g_data.pipe_out[1], TRUE);
-	free_table(table);
+	i_hate_u_norme(table);
 	g_data.line = NULL;
 	free(line);
 	g_data.last_exit_code = 0;
